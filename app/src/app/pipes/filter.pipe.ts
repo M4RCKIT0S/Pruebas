66f5b54c7fa8ab2bf8f5f3d114d1c1fa86890493
M4RCKIT0S <marcosicardo15@gmail.com>
2019-12-04 09:31:17 +0100

    Filtros

--- app/src/app/pipes/filter.pipe.ts
+++ app/src/app/pipes/filter.pipe.ts
@@ -5,13 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, arg: any): any {
+  transform(value: any, args: any): any {
+    if( args === '' || args.length < 2) return value;
     const resultBooks = [];
     for(const book of value){
-      if(book.title.indexOf(arg)>-1){
-        console.log('sip');
-      };
-    };
+      if(book.titulo.toLowerCase().indexOf(args.toLowerCase()) > -1) {
+        resultBooks.push(book);
+      }
+      if(book.ISBN.indexOf(args) > -1){
+        resultBooks.push(book);
+      }
+      if(book.precio.indexOf(args) > -1){
+        resultBooks.push(book);
+      }
+    }
+    return resultBooks;
   }
 
 }
